bb15a193feca81c7e507162986a5fd91abb85c3f
Jujstme <just.tribe@gmail.com>
2022-06-08 20:29:13 +0200

    Aggiungere i file di progetto.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Auto_Recorder
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current Status:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(12, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(289, 30);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please keep this window open. You can minimise it.\r\nClose this window to stop the" +
+    " AutoFirewall.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(109, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "loading...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(322, 111);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Halo Infinite Autofirewall";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,160 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Windows.Forms;
+using LiveSplit.ComponentUtil;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using WindowsFirewallHelper;
+using System.Collections.Generic;
+
+namespace Auto_Recorder
+{
+    class Program
+    {
+        private static MainForm MainForm = new MainForm();
+        private static Process game;
+        private static Watchers watchers;
+
+        private static void Main()
+        {
+            Task task = new Task(() => AutoFirewallTask());
+            task.Start();
+            Application.EnableVisualStyles();
+            Application.Run(MainForm);
+            AllowOutbound();
+        }
+
+        private static void AutoFirewallTask()
+        {
+            while (true)
+            {
+                Thread.Sleep(15);
+
+                if (game == null || game.HasExited)
+                {
+                    if (!HookGameProcess())
+                    {
+                        AllowOutbound();
+                        MainForm.BeginInvoke((MethodInvoker)delegate () { MainForm.label3.Text = "Auto firewall disabled: couldn't connect to the game!"; });
+                        MainForm.BeginInvoke((MethodInvoker)delegate () { MainForm.label4.Text = "not enabled"; });
+                        continue;
+                    }
+                }
+
+                watchers.UpdateAll(game);
+
+                MainForm.BeginInvoke((MethodInvoker)delegate () { MainForm.label3.Text = "Auto firewall enabled!"; });
+
+                if (!watchers.SupportedGameVersion)
+                {
+                    AllowOutbound();
+                    MainForm.BeginInvoke((MethodInvoker)delegate () { MainForm.label3.Text = "Auto firewall disabled: unsupported game version!"; });
+                    MainForm.BeginInvoke((MethodInvoker)delegate () { MainForm.label4.Text = "not enabled"; });
+                    continue;
+                }
+
+                if (!watchers.Connected.Changed && !watchers.FirstUpdate)
+                    continue;
+
+                if (watchers.Connected.Current)
+                {
+                    BlockOutbound();
+                    MainForm.BeginInvoke((MethodInvoker)delegate () { MainForm.label4.Text = "Connections blocked"; });
+                }
+
+                watchers.FirstUpdate = false;
+            }
+        }
+
+        private static void BlockOutbound()
+        {
+            var rule = FirewallManager.Instance.CreateApplicationRule(FirewallProfiles.Domain | FirewallProfiles.Private | FirewallProfiles.Public, "HauloFirewall", FirewallAction.Block, watchers.GamePath);
+            rule.Direction = FirewallDirection.Outbound;
+            FirewallManager.Instance.Rules.Add(rule);
+        }
+
+        private static void AllowOutbound()
+        {
+            var rule = FirewallManager.Instance.Rules.FirstOrDefault(r => r.Name == "HauloFirewall");
+            if (rule != null)
+                FirewallManager.Instance.Rules.Remove(rule);
+        }
+
+             
+        private static bool HookGameProcess()
+        {
+            game = Process.GetProcessesByName("HaloInfinite").OrderByDescending(x => x.StartTime).FirstOrDefault(x => !x.HasExited);
+            if (game == null) return false;
+            
+            try
+            {
+                watchers = new Watchers(game);
+            }
+            catch
+            {
+                game = null;
+                return false;
+            }
+            return true;
+        }
+    }
+
+    class Watchers : MemoryWatcherList
+    {
+        public MemoryWatcher<bool> Connected { get; }
+        public string GamePath { get; }
+        public bool SupportedGameVersion = true;
+        public bool FirstUpdate = true;
+
+        public Watchers(Process game)
+        {
+            var versions = new Dictionary<int, string> {
+                { 0x1263000, "v6.10020.17952.0" }, // Season 1
+                { 0x133F000, "v6.10020.19048.0" },
+                { 0x1262000, "v6.10021.10921.0" },
+                { 0x125D000, "v6.10021.11755.0" },
+                { 0x17F7000, "v6.10021.12835.0" },
+                { 0x1829000, "v6.10021.12835.0" },
+                { 0x1827000, "v6.10021.16272.0" },
+                { 0x17DE000, "v6.10021.18539.0" }, // Season 2
+                { 0x1806000, "v6.10022.10499.0" },
+            };
+            var ArbiterModuleSize = game.ModulesWow64Safe().FirstOrDefault(x => x.ModuleName == "Arbiter.dll").ModuleMemorySize;
+            versions.TryGetValue(ArbiterModuleSize, out var gameVersion);
+            this.GamePath = game.MainModuleWow64Safe().FileName;
+
+            switch (gameVersion)
+            {
+                default:
+                    this.SupportedGameVersion = false;
+                    break;
+                case "v6.10020.17952.0":
+                    this.Connected = new MemoryWatcher<bool>(game.MainModuleWow64Safe().BaseAddress + 0x43265B0);
+                    break;
+                case "v6.10020.19048.0":
+                    // this.Connected = new MemoryWatcher<bool>(game.MainModuleWow64Safe().BaseAddress + 0x47F673C);
+                    this.Connected = new MemoryWatcher<bool>(game.MainModuleWow64Safe().BaseAddress + 0x4FFDD10);
+                    break;
+                case "v6.10021.10921.0":
+                case "v6.10021.11755.0":
+                    this.Connected = new MemoryWatcher<bool>(game.MainModuleWow64Safe().BaseAddress + 0x4327570);
+                    break;
+                case "v6.10021.12835.0":
+                    this.Connected = new MemoryWatcher<bool>(game.MainModuleWow64Safe().BaseAddress + 0x4639C50);
+                    break;
+                case "v6.10021.16272.0":
+                    this.Connected = new MemoryWatcher<bool>(game.MainModuleWow64Safe().BaseAddress + 0x463ED10);
+                    break;
+                case "v6.10021.18539.0":
+                    this.Connected = new MemoryWatcher<bool>(game.MainModuleWow64Safe().BaseAddress + 0x4531C60);
+                    break;
+                case "v6.10022.10499.0":
+                    this.Connected = new MemoryWatcher<bool>(game.MainModuleWow64Safe().BaseAddress + 0x48F6D70);
+                    break;
+            }
+            this.AddRange(this.GetType().GetProperties(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Where(p => !p.GetIndexParameters().Any()).Select(p => p.GetValue(this, null) as MemoryWatcher).Where(p => p != null));
+        }
+    }
+
+}
